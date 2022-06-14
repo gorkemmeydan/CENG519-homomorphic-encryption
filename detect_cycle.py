929c5b14200b6b1763bf0591a8b1063f816306eb
root <gorkem.meydan@hotmail.com>
2022-06-14 21:03:34 +0000

    update results, optimize approach

--- detect_cycle.py
+++ detect_cycle.py
@@ -22,8 +22,9 @@ def generateGraph(n):
 # Two dimensional adjacency matrix is represented as a vector
 # Assume there are n vertices
 # (i,j)th element of the adjacency matrix corresponds to (i*n + j)th element in the vector representations
-def serializeGraphZeroOne(GG,vec_size):
-    n = GG.size()
+def serializeGraphZeroOne(GG,vec_size, node_count):
+    # n = GG.size()
+    n = node_count
     graphdict = {}
     g = []
     for row in range(n):
@@ -53,9 +54,9 @@ def prepareInput(n, m):
     input = {}
     GG = generateGraph(n)
     truth = ground_truth_cycle(GG)
-    graph, graphdict = serializeGraphZeroOne(GG,m)
+    graph, graphdict = serializeGraphZeroOne(GG,m, n)
     input['Graph'] = graph
-    return input, truth
+    return input, truth, graph
 
 # creates possible routes for the cycle
 # @param(size): size of the nodes in the graph
@@ -183,23 +184,153 @@ def simulate(n):
     return compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse
 
 
+######################################3
+import collections
+
+compiler_config = {}
+compiler_config['warn_vec_size'] = 'true'
+compiler_config['lazy_relinearize'] = 'true'
+compiler_config['rescaler'] = 'always'
+compiler_config['balance_reductions'] = 'true'
+
+compiler = CKKSCompiler(config=compiler_config)
+
+def generate_simulation_reqs(node_count, vec_size):
+    eva_prog = EvaProgram("detect_cycle", vec_size=vec_size)
+    
+    with eva_prog:
+        graph = Input('Graph')
+
+        res = []
+        for i in range(node_count):
+            shifted = graph << (i*node_count)
+            res.append(shifted)
+
+        for i in range(node_count):
+            Output(f"Neighbours_{i}", res[i])
+
+    eva_prog.set_output_ranges(30)
+    eva_prog.set_input_scales(30)
+
+    start_time = timeit.default_timer()
+    compiled_func, params, signature = compiler.compile(eva_prog)
+    compilation_time = (timeit.default_timer() - start_time) * 1000.0
+
+    start_time = timeit.default_timer()
+    public_ctx, secret_ctx = generate_keys(params)
+    keygen_time = (timeit.default_timer() - start_time) * 1000.0
+
+    return compiled_func, public_ctx, secret_ctx, signature, compilation_time, keygen_time
+
+def get_neighbours(node_count, node, graph, data_collector, public_ctx,secret_ctx, signature , compiled_func):
+    start_time = timeit.default_timer()
+    enc_inputs = public_ctx.encrypt(graph, signature)
+    encryptiontime = (timeit.default_timer() - start_time) * 1000.0
+
+    start_time = timeit.default_timer()
+    enc_outputs = public_ctx.execute(compiled_func, enc_inputs)
+    executiontime = (timeit.default_timer() - start_time) * 1000.0
+
+    start_time = timeit.default_timer()
+    outputs = secret_ctx.decrypt(enc_outputs, signature)
+    decryptiontime = (timeit.default_timer() - start_time) * 1000.0
+
+    start_time = timeit.default_timer()
+    reference = evaluate(compiled_func, graph)
+    referenceexecutiontime = (timeit.default_timer() - start_time) * 1000.0
+
+    mse = valuation_mse(outputs, reference)
+
+    data_collector["encryptiontime"].append(encryptiontime)
+    data_collector["executiontime"].append(executiontime)
+    data_collector["referenceexecutiontime"].append(referenceexecutiontime)
+    data_collector["decryptiontime"].append(decryptiontime)
+    data_collector["mse"].append(mse)
+
+    # tidy output
+    raw_output = outputs[f"Neighbours_{node}"]
+    threshold = 0.5
+    neighbours = []
+
+    for i in range(node_count):
+        if (i == node):
+            continue
+        if raw_output[i] > threshold:
+            neighbours.append(i)      
+
+    return neighbours
+
+def cycle_helper(node_count, current, visited, parent, graph, data_collector, public_ctx,secret_ctx, signature , compiled_func):
+    visited[current] = True
+
+    neighbours = get_neighbours(node_count, current,graph, data_collector, public_ctx,secret_ctx,  signature , compiled_func)
+
+    for n in neighbours:
+        if visited[n] == False:
+            if cycle_helper(node_count, n, visited, current, graph, data_collector, public_ctx,secret_ctx, signature , compiled_func):
+                return True
+        elif parent != n:
+            return True
+    return False
+
+def make_simulation(node_count):
+    # compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse
+
+    compiled_func, public_ctx, secret_ctx, signature, compilation_time, keygen_time = generate_simulation_reqs(node_count, 4096)
+
+    data_collector = collections.defaultdict(list)
+    data_collector["compiletime"].append(compilation_time)
+    data_collector["keygenerationtime"].append(keygen_time)
+
+    graph, truth, graph_arr = prepareInput(node_count, 4096)
+
+    visited = [False]*(node_count)
+
+    result = False
+    for i in range(node_count):
+        if visited[i] == False:
+            if cycle_helper(node_count, i, visited, -1, graph, data_collector, public_ctx,secret_ctx, signature , compiled_func ):
+                result = True
+    
+    return result, truth, data_collector
+
+def process_data_collector(data_collector):
+    compiletime = data_collector["compiletime"][0]
+    keygenerationtime = data_collector["keygenerationtime"][0]
+    encryptiontime =0 
+    executiontime = 0 
+    referenceexecutiontime = 0
+    decryptiontime = 0
+    mse = 0
+    for x in data_collector["encryptiontime"]:
+        encryptiontime += x
+    for x in data_collector["executiontime"]:
+        executiontime += x
+    for x in data_collector["decryptiontime"]:
+        decryptiontime += x
+    for x in data_collector["referenceexecutiontime"]:
+        referenceexecutiontime += x
+    for x in data_collector[mse]:
+        mse += x
+    mse = mse / len(data_collector["mse"])
+    
+
+    return compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse
+
 if __name__ == "__main__":
-    simcnt = 50 #The number of simulation runs, set it to 3 during development otherwise you will wait for a long time
-    # For benchmarking you must set it to a large number, e.g., 100
+    simcnt = 100
+
     #Note that file is opened in append mode, previous results will be kept in the file
     resultfile = open("results.csv", "a")  # Measurement results are collated in this file for you to plot later on
     resultfile.write("NodeCount,SimCnt,CompileTime,KeyGenerationTime,EncryptionTime,ExecutionTime,DecryptionTime,ReferenceExecutionTime,Mse\n")
-    resultfile.close()
-    
+
     print("Simulation campaing started:")
-    for nc in range(4,5,1): # Node counts for experimenting various graph sizes
-        n = nc
-        resultfile = open("results.csv", "a") 
+    for nc in [4,8,16,32]: # Node counts for experimenting various graph sizes
+        node_count = nc
         for i in range(simcnt):
-            print("sim set ", i," out of ", simcnt)
-            #Call the simulator
-            compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse = simulate(n)
-            res = str(n) + "," + str(i) + "," + str(compiletime) + "," + str(keygenerationtime) + "," +  str(encryptiontime) + "," +  str(executiontime) + "," +  str(decryptiontime) + "," +  str(referenceexecutiontime) + "," +  str(mse) + "\n"
-            # print(res)
+            print("node count: ", nc, " step: ", i)
+            result, truth, data_collector = make_simulation(node_count)
+            compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse = process_data_collector(data_collector)
+            res = str(node_count) + "," + str(i) + "," + str(compiletime) + "," + str(keygenerationtime) + "," +  str(encryptiontime) + "," +  str(executiontime) + "," +  str(decryptiontime) + "," +  str(referenceexecutiontime) + "," +  str(mse) + "\n"
             resultfile.write(res)
-        resultfile.close()
+    resultfile.close()
